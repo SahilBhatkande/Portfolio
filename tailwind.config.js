ace861246a762ca2d75e4daec59dfa8d7186899c
sahil07-git <sahilbhatkande231@gmail.com>
2025-06-09 19:36:31 +0530

    Updated PostCSS config and Tailwind CSS setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
+// /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         'gray-850': '#112240',
+        bg: '#0A0B0D',
+        primary: '#00F0FF',
+        secondary: '#7000FF',
       },
     },
   },
   plugins: [],
 };
+
